6d1464d2a592412d9ef8838694aa6748c7e7f477
壮 苗 <doublezhuang101@hotmail.com>
2019-01-27 22:11:14 +0800

    第一天

--- C语言程序设计/C语言课程设计.c
+++ C语言程序设计/C语言课程设计.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
-int main()
-{
+#define line 14
+void square(void);//打印line数量个方框
 
+void main()
+{
+	printf("		学生信息信息管理系统");
+	printf("	制作人;苗壮");
+
+	getchar();
+}
+void square(void)
+{
+	int i = 0;
+	for (i = 0; i < line; i++)
+		printf("%2c", 3);
+	printf("\n");
 }
