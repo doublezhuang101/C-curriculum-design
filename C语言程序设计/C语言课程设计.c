be860b58cf93eac576ea72161fc6c054a42c78c7
壮 苗 <doublezhuang101@hotmail.com>
2019-02-09 11:21:31 +0800

    删除完成

--- C语言程序设计/C语言课程设计.c
+++ C语言程序设计/C语言课程设计.c
@@ -7,9 +7,13 @@
 void square(void);//打印line数量个方框
 void menu(void);//开始菜单
 void typein(void);//学生信息录入
+void save(void);//保存学生信息
+void deletemenu(void);//删除学生信息菜单
+void delete_number(void);//按学号删除学生信息
 void rank1(void);//冒泡排序按学号
 void rank2(void);//选择排序按高数成绩排序
 void rank3(void);//快速排序按英语成绩排序
+void kuaipai(void);//快速排序主函数
 void rank4(void);//插入排序按C语言成绩排序
 void rank5(void);//快速排序按体育成绩排序
 void rank6(void);//快速排序按课程设计成绩排序
@@ -63,12 +67,19 @@ void menu()
 	printf("	9.退出系统\n");
 	square();
 	printf("	请输入数字进行功能选择:");
-	scanf_s("%d",&choice);
+	scanf("%d",&choice);
 	getchar();
 	switch (choice)
 	{
 	case 1:
 		typein();
+		break;
+	case 2:
+		save();
+		break;
+	case 3:
+		deletemenu();
+		break;
 	default:
 		system("CLS");
 		printf("	输入选项错误！请重新输入\n");
@@ -107,6 +118,55 @@ void typein()
 	scanf("%d", &student[count].score5);
 	getchar();
 	count++;
+}
+void save()
+{
+
+}
+void deletemenu(void)
+{
+	int c;
+	square();
+	printf("	输入数字进行功能选择:\n");
+	printf("	1.输入学号选择删除条目\n");
+	printf("	2.取消删除返回上一级菜单\n");
+	square();
+	printf("请输入数字选择：");
+	scanf("%d", &c);
+	getchar();
+	switch (c)
+	{
+	case 1:
+		delete_number();
+		break;
+	case 2:
+		menu();
+		break;
+	default:
+		printf("输入错误，请重新输入");
+		deletemenu();
+	}
+}
+void delete_number(void)
+{
+	long int number;
+	int i, j;
+	int match=0;
+	printf("请输入学生学号:");
+	scanf("%ld", &number);
+	for (i = 0; i <= count; i++)
+		if (student[i].number == number)
+		{
+			for (j = i; j < count; j++)
+				student[j].number = student[j + 1].number;
+			printf("已经成功删除！");
+			match++;
+		}
+	if (match == 0)
+	{
+		printf("无法查到相关数据\n");
+		deletemenu();
+	}
 }
 void rank1(void)//冒泡排序
 {
@@ -159,33 +219,17 @@ void rank2(void)//选择排序
 }
 void rank3(void)//快速排序
 {
-	int i=0, j=count;    //定义变量为基本整型
-    //将每组首个元素赋给i
-	//将每组末尾元素赋给j
-	s[0] = s[max];    //设置基准值
-	while (i < j)
-	{
-		while (i < j&&s[0] < s[j])
-			j--;    //位置左移
-		if (i < j)
-		{
-			s[i] = s[j];    //将s[j]放到s[i]的位置上
-			i++;    //位置右移
-		}
-		while (i < j&&s[i] <= s[0])
-			i++;    //位置左移
-		if (i < j)
-		{
-			s[j] = s[i];    //将大于基准值的s[j]放到s[i]位置
-			j--;    //位置左移
-		}
-	}
+	
 	printf("\n学号\t姓名\t性别\t年龄\t手机号码   高等数学成绩   英语成绩   C语言成绩   体育成绩   课程设计成绩\n");
-	for (i = 0; i < count; i++)
+	for (int i = 0; i < count; i++)
 		printf("\n%ld\t%s\t%s\t%d\t%s   %6d   %4d   %4d   %4d   %6d ", student[i].number, student[i].name, student[i].sex,
 			student[i].age, student[i].phone, student[i].score1, student[i].score2, student[i].score3, student[i].score4, student[i].score5);
 }
+void kuaipai()
+{
+
+}
 void exit()
 {
-	我摸
+	
 }
